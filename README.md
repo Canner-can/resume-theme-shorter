5a08ca37a9a68dacabc3a65d0024bfd441eee0b9
howardchi <howardchi@fougood.com>
2015-06-21 17:57:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# resume-theme-flat
+
+resume from JSONresume the flat, original repo 
+
+https://github.com/erming/jsonresume-theme-flat
+
+[Theme Preview](http://themes.jsonresume.org/flat)
+
+## License
+
+MIT
---
+++ gulpfile/watch.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var config = require('./config'),
+    watch = require('gulp-watch');
+
+gulp.task('watch', function() {
+    gulp.watch(config.scss_files, ['scss']);
+    gulp.watch(config.views.hbs, ['canner']);
+});
